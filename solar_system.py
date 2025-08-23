fee827fa106384857a29e1e7124f31cf7c28d936
Steve <stevelopez290519@gmail.com>
2025-08-22 21:12:37 -0500

    ML-Challenge2

--- solar_system.py
+++ solar_system.py
@@ -19,5 +19,52 @@ def getData():
         print(data)
     except requests.exceptions.RequestException as e:
         print(f"API error: {e}")
-#Call main function
-getData()
+
+def showPlanets():
+    url = "https://api.le-systeme-solaire.net/rest/bodies/"
+    data = requests.get(url).json()
+    print("\n::: PLANETS :::")
+    for b in data["bodies"]:
+        if b["isPlanet"]:
+            print("-", b["englishName"])
+
+def showMoons():
+    url = "https://api.le-systeme-solaire.net/rest/bodies/"
+    data = requests.get(url).json()
+    print("\n::: MOONS :::")
+    for b in data["bodies"]:
+        if b.get("aroundPlanet"):
+            print("-", b["englishName"])
+
+def showComets():
+    url = "https://api.le-systeme-solaire.net/rest/bodies/?filter[]=isComet,eq,true"
+    data = requests.get(url).json()
+    print("\n::: COMETS :::")
+    for b in data.get("bodies", []):
+        print("-", b["englishName"])
+
+def mainMenu():
+    while True:
+        print("\n:::: MAIN MENU ::::")
+        print("1. Planetas")
+        print("2. Satélites")
+        print("3. Cometas")
+        print("4. Salir")
+
+        choice = input("Seleccione una opción: ")
+        if choice == "1":
+            showPlanets()
+        elif choice == "2":
+            showMoons()
+        elif choice == "3":
+            showComets()
+        elif choice == "4":
+            print("Saliendo...")
+            break
+        else:
+            print("Opción inválida")
+
+
+# #Call main function
+# getData()
+mainMenu()
